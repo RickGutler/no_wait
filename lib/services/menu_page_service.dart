a422cec9cec799e655585a040ffd384a56df3f8b
Henrique G. Prezotti <henriqueprezotti@gmail.com>
2024-09-04 18:18:56 -0300

    Produtos na tela do menu
    -Falta fazer o shimmer de carregamento

--- lib/services/menu_page_service.dart
+++ lib/services/menu_page_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:no_wait/models/categoria.dart';
+import 'package:no_wait/models/produto.dart';
 
 class MenuPageService extends ChangeNotifier {
   static final MenuPageService _instance = MenuPageService._internal();
@@ -23,6 +24,22 @@ class MenuPageService extends ChangeNotifier {
       return categorias;
     } catch (e) {
       print('Erro ao buscar categorias: $e');
+      return [];
+    }
+  }
+
+  Future<List<Produto>> buscarProdutos() async {
+    try {
+      final QuerySnapshot snapshot =
+          await FirebaseFirestore.instance.collection('produto').get();
+
+      final List<Produto> produtos = snapshot.docs.map((doc) {
+        return Produto.fromJson(doc.data() as Map<String, dynamic>);
+      }).toList();
+
+      return produtos;
+    } catch (e) {
+      print('Erro ao buscar produto: $e');
       return [];
     }
   }
