4ff61dbc29f28a0bf6e454d48676ac8ffa5c8873
Henrique G. Prezotti <henriqueprezotti@gmail.com>
2024-09-04 16:20:19 -0300

    Appbar do menu

--- lib/pages/menu_page/menu_page.dart
+++ lib/pages/menu_page/menu_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:no_wait/components/app_bar_customizada_menu.dart';
 import 'package:no_wait/components/botao_categoria.dart';
 import 'package:no_wait/pages/menu_page/menu_page_controller.dart';
 import 'package:no_wait/style/app_colors.dart';
@@ -22,26 +23,8 @@ class _MenuPageState extends State<MenuPage> {
 
   @override
   Widget build(BuildContext context) {
-    double paddingTop = MediaQuery.of(context).padding.top;
-
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(paddingTop + 50),
-          child: SizedBox(
-            height: paddingTop + 50,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: paddingTop),
-                Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: SizedBox(
-                      height: 25, child: Image.asset("assets/logo.png")),
-                ),
-              ],
-            ),
-          )),
+      appBar: const AppBarCustomizadaMenu(),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
