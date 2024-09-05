dc782e6969901b04036ee4ec1eeb5dd840c3b026
RickGutler <rickgutler42@gmail.com>
2024-09-05 08:15:47 -0300

    Inter Font

--- lib/style/app_fonts.dart
+++ lib/style/app_fonts.dart
@@ -3,13 +3,13 @@ import 'package:no_wait/style/app_colors.dart';
 
 class AppFonts {
   static const TextStyle titleSmall = TextStyle(
-      fontFamily: 'inter',
+      fontFamily: 'Inter',
       fontSize: 12,
       fontWeight: FontWeight.bold,
       color: AppColors.corPrincipal);
 
   static const TextStyle headlineSmall = TextStyle(
-      fontFamily: 'inter',
+      // fontFamily: 'Inter',
       fontSize: 30,
       fontWeight: FontWeight.bold,
       color: AppColors.corPrincipal);
